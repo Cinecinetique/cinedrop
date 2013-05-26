13b31ab98b120dac3dbd60e084fe837e7d46d7b1
Rija Ménagé <git@rijam.sent.as>
2013-05-26 17:57:24 +0200

    First version of cinecinetique web application. Set up wtih a hello world view from static controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cinecinetique::Application.config.secret_key_base = 'fdc8f46e065fc69ed69decaa61b68cf4ef0b08b405d0de6b4ffab2d29c7438cb02e045e4f9f2e314bb7473db3b6b661b5169cb744f194829aadf596c3199f1f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cinecinetique::Application.config.session_store :cookie_store, key: '_cinecinetique_session'
