7f4e636199a8112dcff7c3a62225085c24f8a63b
Rija Ménagé <git@rijam.sent.as>
2013-07-08 16:48:36 +0800

    Added a VideoThumbnail processor for paperclip. Added AWS Elasticbeanstalk config to enable epel, centos and rpmforge repo on the EC2 instance and install ffmpeg

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,47 @@
 class Document < ActiveRecord::Base
-	has_attached_file :data, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :data, 
+                    :styles => { :medium => ["300x300>",:jpg], :thumb => ["100x100>",:jpg] },
+					:default_url => "/images/:style/missing.png",
+					:processors => lambda { |a| a.video? ? [ :video_thumbnail ] : [ :thumbnail ] }
+
+
+	def video?
+    [ 'application/x-mp4',
+      'video/mpeg',
+      'video/quicktime',
+      'video/x-la-asf',
+      'video/x-ms-asf',
+      'video/x-msvideo',
+      'video/x-sgi-movie',
+      'video/x-flv',
+      'flv-application/octet-stream',
+      'video/3gpp',
+      'video/3gpp2',
+      'video/3gpp-tt',
+      'video/BMPEG',
+      'video/BT656',
+      'video/CelB',
+      'video/DV',
+      'video/H261',
+      'video/H263',
+      'video/H263-1998',
+      'video/H263-2000',
+      'video/H264',
+      'video/JPEG',
+      'video/MJ2',
+      'video/MP1S',
+      'video/MP2P',
+      'video/MP2T',
+      'video/mp4',
+      'video/MP4V-ES',
+      'video/MPV',
+      'video/mpeg4',
+      'video/mpeg4-generic',
+      'video/nv',
+      'video/parityfec',
+      'video/pointer',
+      'video/raw',
+      'video/rtx' ].include?(data.content_type)
+  end
+
 end
