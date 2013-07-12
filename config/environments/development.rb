09ff3ee8ee081b63121640e1b77ba896078ca213
Rija Ménagé <git@rijam.sent.as>
2013-07-12 16:13:28 +0800

    make sure AWS and paperclip are set up with production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,11 +32,11 @@ Cinecinetique::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_host_name => "s3-ap-southeast-1.amazonaws.com",
-    :s3_endpoint => "s3-ap-southeast-1.amazonaws.com"
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_host_name => "s3-ap-southeast-1.amazonaws.com",
+      :s3_endpoint => "s3-ap-southeast-1.amazonaws.com"
+    }
   }
-}
 end
