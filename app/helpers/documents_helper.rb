29687b1526fe849af115a40704468d8927a5bb97
Rija Ménagé <git@rijam.sent.as>
2013-08-21 17:37:36 +0800

    use S3 signed urls to pass to the google docs viewer

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,11 @@
 module DocumentsHelper
+	def signed_url(document)
+		policy = AWS::STS::Policy.new
+		policy.allow(:actions => :any, :resource => :any)
+		session = AWS::STS.new.new_federated_session('mysession', :policy => policy)
+
+		s3 = AWS::S3.new(session.credentials)
+		url = s3.buckets[ENV['AWS_BUCKET']].objects[document.data.path[1..-1]].url_for(:read)
+		url.to_s
+	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,13 @@ Cinecinetique::Application.configure do
 
 
   config.paperclip_defaults = {
-      :storage => :filesystem
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+        :s3_host_name => "s3-ap-southeast-1.amazonaws.com",
+        :s3_endpoint => "s3-ap-southeast-1.amazonaws.com"
+      }
   }
 end
