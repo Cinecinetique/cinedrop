c46ad7e0f82cc866a2b4f3cd4d2aee7e2fb5cce9
Rija Ménagé <git@rijam.sent.as>
2013-08-22 16:59:05 +0800

    adjust thumbnail size for vimeo video to match the youtube thumbnail size

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,6 +3,6 @@ module ProjectsHelper
 		vimeo_video_json_url     = "http://vimeo.com/api/v2/video/%s.json" % document.data_ref   # API call
 
 		# Parse the JSON and extract the thumbnail_large url
-		thumbnail_image_location = JSON.parse(open(vimeo_video_json_url).read).first['thumbnail_small'] rescue nil
+		thumbnail_image_location = JSON.parse(open(vimeo_video_json_url).read).first['thumbnail_medium'] rescue nil
 	end
 end
