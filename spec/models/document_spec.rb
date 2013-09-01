e25d3f4a528d83128807c9435ccc91943f73ca45
Rija Ménagé <git@rijam.sent.as>
2013-09-01 19:14:16 +0800

    Added some help text to the document upload form and fixed tests

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -34,14 +34,20 @@ describe Document, "When a new document is created without file attachement" do
 	end
 
 	it "should save if the name is supplied"  do 
+		user = mock()
+		changes_count = mock()
 		parameters = {
 			name: "a name",
 		}
+		changes_count.expects(:success?).returns(false)
+		Firebase.expects(:get).with("changes_count").returns(changes_count)
 		document = Document.new(parameters)
 		expect { document.save! }.to_not raise_error
 	end
 
 	it "should not save if data_host is not in the list of accepted services" do 
+		user = mock()
+		changes_count = mock()
 		parameters_notok = {
 			name: "a name",
 			data_host: "xxx",
@@ -52,19 +58,25 @@ describe Document, "When a new document is created without file attachement" do
 			name: "a name",
 			data_host: "vimeo",
 			data_ref: "yyy",
+			changed_by: 4,
 		}
 		parameters_ok[1] = {
 			name: "a name",
 			data_host: "youtube",
 			data_ref: "yyy",
+			changed_by: 4,
 		}		
 		document = Document.new(parameters_notok)
 		expect { document.save! }.to raise_error(ActiveRecord::RecordInvalid)
 
 		parameters_ok.each do |parameter_ok|
+			changes_count.expects(:success?).returns(false)
+			Firebase.expects(:get).with("changes_count").returns(changes_count)
 			document = Document.new(parameter_ok)
 			expect { document.save! }.to_not raise_error
 		end
-
 	end
+	it "should not trigger a notification to firebase if query for changes count is not sucessful"
+
+	it "should trigger a push of change notification to firebase if changes count exists"
 end
