0d920cee6ee55cca25fef92f13dab88c0d83efcf
Rija Ménagé <git@rijam.sent.as>
2013-10-08 17:59:22 +0800

    whitelist static css for precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Cinecinetique::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile += ['static.css.scss'] 
+  config.assets.precompile += ['static.css'] 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
