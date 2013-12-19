e8f333d830b79517df38f8977815ed61c751d10d
Rija Ménagé <git@rijam.sent.as>
2013-12-19 17:45:33 +0800

    made the drop table subscriptions migration conditional

--- db/migrate/20131119112737_drop_old_subscriptions.rb
+++ db/migrate/20131119112737_drop_old_subscriptions.rb
@@ -1,10 +1,12 @@
 class DropOldSubscriptions < ActiveRecord::Migration
   def up
-  	drop_table :subscriptions
+  	unless table_exists?(:subscriptions)
+	  	drop_table :subscriptions
+		end
   end
 
 	def down
-  	raise ActiveRecord::IrreversibleMigration
-  end
+	  	raise ActiveRecord::IrreversibleMigration
+	end
 
 end
