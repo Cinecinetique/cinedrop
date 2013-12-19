7d671e99c0f01f86a4937712a76841d8b61fded8
Rija Ménagé <git@rijam.sent.as>
2013-12-19 19:33:27 +0800

    adding some debug info

--- app/models/paypal_notification.rb
+++ app/models/paypal_notification.rb
@@ -2,6 +2,7 @@ class PaypalNotification < ActiveRecord::Base
 	
 	def self.validate_IPN_notification(raw)
       uri = URI.parse(::IPN_URL)
+      Rails.logger.info "About to connect to Paypal IPN: #{uri.host}:#{uri.port}"
       http = Net::HTTP.new(uri.host, uri.port)
       http.open_timeout = 60
       http.read_timeout = 60
@@ -11,5 +12,6 @@ class PaypalNotification < ActiveRecord::Base
                            'Content-Length' => "#{raw.size}",
                            'User-Agent' => "My custom user agent"
                          ).body
+      Rails.logger.info "Response from Paypal IPN: #{response}"
     end
 end
