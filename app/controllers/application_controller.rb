e213a49a2b43e437335c56d08a8f78c80336875a
Rija Ménagé <git@rijam.sent.as>
2014-01-06 11:22:52 +0800

    added a log entry for silent redirect due to unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to plans_url #, :alert => exception.message
+    Rails.logger.info "#{exception.message} during attempt of #{exception.action} on #{exception.subject} by user #{current_user.try(:id)}"
   end
 
 
